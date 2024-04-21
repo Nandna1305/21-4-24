6c49ee7cfa714d09e79edf7c7c9d8c52f27ab2c6
unknown <tommyandjerry1330@gmail.com>
2024-04-21 07:37:18 +0530

    added file jasmine.java

---
+++ jasmine.java
@@ -0,0 +1,18 @@
+class jasmine {
+    public static void main(String args[]) {
+        int score = 35;
+        if(score>30 && score<60 )
+        {
+            System.out.println("videogames");
+        }
+        //using else if statement
+        else if (score>60 && score<80)
+        {
+            System.out.println("iphone");
+        }
+        if(score>80)
+        {
+            System.out.println("macbook");
+        }
+    }
+}
