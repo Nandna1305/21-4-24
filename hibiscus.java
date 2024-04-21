a2877d0cd4cd1faf4cc1e5e45c5ceff34c6102cc
unknown <tommyandjerry1330@gmail.com>
2024-04-21 08:56:42 +0530

    added file hibiscus.java

---
+++ hibiscus.java
@@ -0,0 +1,8 @@
+public class hibiscus {
+    public static void main(String args[]) {
+        boolean rain = true;
+        String result = rain?"take an umbrella":"enjoy the sunlight";
+        System.out.println(result);
+    }
+    
+}
