11f96e4dab1f7772cea99b13c2bad0a916b537b8
unknown <tommyandjerry1330@gmail.com>
2024-04-21 07:57:32 +0530

    added file lily.java

---
+++ lily.java
@@ -0,0 +1,19 @@
+public class lily {
+    public static void main (String args[]) {
+        int score = 75;
+        if(score<50)
+        {
+            System.out.println("you need to improve");
+        }
+        else if (score>=50 && score<=70)
+        {
+            System.out.println("good job");
+
+        }
+        if(score>70)
+        {
+            System.out.println("good performance");
+        }
+    }
+    
+}
