8e447151079144c0a4b2c2123f4c5ec472cefe6d
unknown <tommyandjerry1330@gmail.com>
2024-04-21 07:47:55 +0530

    added file lotus.java

---
+++ lotus.java
@@ -0,0 +1,20 @@
+public class lotus {
+    public static void main(String args[]) {
+        boolean marks = true;
+        boolean medicine = true;
+        boolean doctor = true;
+        //using nested-if statement
+        if(marks)
+        {
+            System.out.println("chances are there to get a medical seat");
+        }
+        if(medicine)
+        {
+            System.out.println("you would be getting on youe neet score");
+        }
+        if(doctor)
+        {
+            System.out.println("you are going to become a famous doctor");
+        }
+    }   
+}
